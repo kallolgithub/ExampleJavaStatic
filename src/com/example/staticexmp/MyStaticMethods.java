68d82c8bc5d12c5e5f4ab38a94b2ce9461212e69
kallolgithub <kallolgithub@gmail.com>
2014-07-31 01:56:58 +0600

    Initial commit

---
+++ src/com/example/staticexmp/MyStaticMethods.java
@@ -0,0 +1,26 @@
+package com.example.staticexmp;
+
+/**
+ * Created by kallol on 7/31/14.
+ */
+public class MyStaticMethods {
+
+    private String name;
+    private static String staticStr = "STATIC-STRING";
+
+    public MyStaticMethods(String n) {
+        this.name = n;
+    }
+
+    public static void testStaticMethod() {
+
+    }
+
+    public void testObjectMethod() {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
